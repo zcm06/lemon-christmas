d9ee6d800fa5feac459e3dd727ac18202916590d
张灿明 <635677619@qq.com>
2019-12-18 23:40:56 +0800

    init

---
+++ src/main/java/com/lemon/christmas/ChristmasApplication.java
@@ -0,0 +1,13 @@
+package com.lemon.christmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasApplication.class, args);
+	}
+
+}
